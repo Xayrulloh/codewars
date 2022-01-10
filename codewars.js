d61f521ad613cc6343457d64f0a7de5ee7ca096d
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2022-01-10 18:09:44 +0500

    new code wars exercises completed

--- codewars.js
+++ codewars.js
@@ -6,9 +6,13 @@ console.log(shortcut("goodbye"));*/
 /*const uniTotal = str => [...str].reduce((acc, char) => acc + char.charCodeAt(0), 0);
 console.log(uniTotal('aaa'));*/
 
-// 
+// Points of Reflection
+/*const symmetricPoint = ([a, b], [c, d]) => [c * 2 - a, d * 2 - b]
+console.log(symmetricPoint([10, -10], [-10, 10]));*/
 
-
+// Fake Binary
+/*const fakeBin = str => str.replace(/[1234]/g, '0').replace(/[56789]/g, '1')
+console.log(fakeBin('45385593107843568'));*/
 
 
 
