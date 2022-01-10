b063b0724ca0f181ef075a07f9d385cc9ff60cbf
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2022-01-10 17:32:31 +0500

    new code wars exercises completed

---
+++ codewars.js
@@ -0,0 +1,20 @@
+// Vowel remover
+/*const shortcut = str => {for (let a = 0; a < str.length; a++) if ('euioa'.includes(str[a])) {str = str.slice(0, a) + str.slice(a + 1); a--}; return str}
+console.log(shortcut("goodbye"));*/
+
+// ASCII Total
+/*const uniTotal = str => [...str].reduce((acc, char) => acc + char.charCodeAt(0), 0);
+console.log(uniTotal('aaa'));*/
+
+// 
+
+
+
+
+
+
+
+
+
+
+
